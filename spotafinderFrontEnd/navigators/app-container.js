34e657424d1ca47f8bf70520fd5cc58f7eb908a3
Khai <zekhai@mail.fresnostate.edu>
2019-09-18 00:40:39 -0700

    Auth working

--- spotafinderFrontEnd/navigators/app-container.js
+++ spotafinderFrontEnd/navigators/app-container.js
@@ -4,15 +4,17 @@ import {createAppContainer, createSwitchNavigator} from 'react-navigation'
 import Home from '../scenes/home-scene';
 import Library from '../scenes/library-secne';
 import Login from '../scenes/login-scene';
+import SignUp from '../scenes/sign-up-scene';
 
 const SwitchNavigator = createSwitchNavigator(
     {
+        SignUp: SignUp,
         Login: Login,
         Home: Home,
         Library: Library,
     },
     {
-        initialRouteName: 'Login',
+        initialRouteName: 'SignUp',
     },
 );
 
--- spotafinderFrontEnd/scenes/sign-up-scene.js
+++ spotafinderFrontEnd/scenes/sign-up-scene.js
@@ -1,26 +1,35 @@
 import React, { useState, useEffect } from 'react'
 import { View, TextInput, StyleSheet, Dimensions, Button } from 'react-native';
 import NavigatorService from '../navigator-service';
+import FireBase from '../firebase';
 
-export default function LoginScene() {
-    const [user, userInput] = useState('');
-    const [password, passwordInput] = useState('');
+async function createAccount(user, password) {
+    try {
+        await FireBase.auth().createUserWithEmailAndPassword(user, password);
+    } catch (error) {
+        console.log(error.toString())
+    }
+}
 
-    return (
+export default function Register() {
+    const [newUser, inputUser] = useState('');
+    const [newPassword, inputPassword] = useState('');
+
+    return(
         <View style = {styles.container}>
             <TextInput
                 style = {styles.box}
-                placeholder = 'Type your username!'
-                onChangeText = {user => userInput(user)}
+                placeholder = 'Create User!'
+                onChangeText = {newUser => inputUser(newUser)}
             />
             <TextInput
                 style = {styles.box}
-                placeholder = 'Type your password!'
-                onChangeText = {password => passwordInput(password)}
+                placeholder = 'Create Password'
+                onChangeText = {newPassword => inputPassword(newPassword)}
             />
             <Button
-                title = 'Login'
-                onPress = {() => NavigatorService.navigation('Home', {Home: 'Home'})}
+                title = 'Register Done!'
+                onPress = {() => { NavigatorService.navigation('Home', {Home: 'Home'}); createAccount(newUser, newPassword) }}
             />
         </View>
     );
