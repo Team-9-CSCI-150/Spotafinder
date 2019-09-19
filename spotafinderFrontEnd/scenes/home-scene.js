fb08168c653d8fe807bb359db246f8d015c0b71c
Khai <zekhai@mail.fresnostate.edu>
2019-09-18 18:41:01 -0700

    Merge complete

---
+++ spotafinderFrontEnd/scenes/home-scene.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Text, View, Button} from 'react-native';
+import NavigatorService from '../navigator-service';
+
+export default function HomeScene() {
+    //Possible need a hook
+    return (
+        <View>
+            <Button 
+                title = "Home"
+                color = "red"
+                onPress = {() => NavigatorService.navigation('Library', {Library: 'Library'})}
+            />
+        </View>
+    );
+}
