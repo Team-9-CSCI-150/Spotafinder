7daf697f58c80e46ac36979f16ee1f5c44b22b05
Trien Ngo <trien.ngo559@gmail.com>
2019-10-08 15:40:35 -0700

    clearing frontend for restart
    clearing folder so its clearer for future use

--- spotafinderFrontEnd/style.js
+++
@@ -1,7 +0,0 @@
-import { StyleSheet, Dimensions } from 'react-native';
-
-export let Window_Width  = Dimensions.get('window').width;
-export let Window_Height = Dimensions.get('window').height;
-
-export let Icon = require('./images/icon.png');
-
