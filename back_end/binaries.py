b909ad88613918caa0cfbb19d259e0736c6a08e8
Trien Ngo <trien.ngo559@gmail.com>
2019-11-05 14:53:01 -0800

    notes

--- back_end/binaries.py
+++ back_end/binaries.py
@@ -4,3 +4,6 @@ import util
 ariodump_ng = util.which('airodump-ng')
 aircrack_ng = util.which('aircrack-ng')
 airmon_ng = util.which('airmon-ng')
+
+# airodump-ng -w myOutput --output-format csv mon0
+# command to write output to a .csv file
