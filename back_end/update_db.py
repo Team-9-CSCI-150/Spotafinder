a4dc35a0920201e30fe0599ecad26f9a5f1e2d40
Brian Cardwell <bcardwell86@gmail.com>
2019-11-18 11:42:30 -0800

    Update update_db.py

--- back_end/update_db.py
+++ back_end/update_db.py
@@ -1,9 +1,21 @@
-#updates database every 10 seconds with random data
-
 import pyrebase
 from random import seed
 from random import randint
 import time
+
+class updateOccupancy:
+
+	def __init__(self, EEdata, Libdata):
+		self.EEdata = EEdata
+		self.Libdata = Libdata
+		
+	def send(data):
+		EEdb.update(data.EEdata)
+		Ldb.update(data.Libdata)
+		
+
+    
+
 
 config = {
   "apiKey": "AIzaSyAzclOJusoyXpvyB9GHpk4_3ddwtOm-36w",
@@ -16,20 +28,22 @@ firebase = pyrebase.initialize_app(config)
 EEdb = firebase.database()
 Ldb = firebase.database()
 
+EEdata = {}
+Libdata = {}
+c = updateOccupancy(EEdata, Libdata)
+
 seed(1)
 for _ in range(10):
-    ee281occ = randint(0,20)
-    ee283occ = randint(0,20)
-    lfloor1 = randint(0,100)
-    lfloor2 = randint(0,100)
+	
+	ee281occ = randint(0,20)
+	ee283occ = randint(0,20)
+	lfloor1 = randint(0,100)
+	lfloor2 = randint(0,100)
 
-    EEdata = {"Engineering East/rooms/EE281":{"occupancy": ee281occ},
-        "Engineering East/rooms/EE283":{"occupancy": ee283occ}}
-    EEdb.update(EEdata)
-    Ldata = {"Library/floors/1":{"occupancy": lfloor1},
-        "Library/floors/2":{"occupancy": lfloor2}}
-    Ldb.update(Ldata)
-    time.sleep(10)
-    
-
-
+	c.EEdata = {"Engineering East/rooms/EE281":{"occupancy": ee281occ},
+		"Engineering East/rooms/EE283":{"occupancy": ee283occ}}
+	
+	c.Libdata = {"Library/floors/1":{"occupancy": lfloor1},
+		"Library/floors/2":{"occupancy": lfloor2}}
+	c.send()
+	time.sleep(5)
