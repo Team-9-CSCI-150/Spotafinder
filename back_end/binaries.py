68845051cddf8c4af8a837489afa46de20ed2737
Trien Ngo <trien.ngo559@gmail.com>
2019-11-24 14:48:43 -0800

    adding more utils

--- back_end/binaries.py
+++ back_end/binaries.py
@@ -4,6 +4,7 @@ import util
 ariodump_ng = util.which('airodump-ng')
 aircrack_ng = util.which('aircrack-ng')
 airmon_ng = util.which('airmon-ng')
+wifite = util.which('wifite')
 
 # airodump-ng -w myOutput --output-format csv mon0
 # command to write output to a .csv file
