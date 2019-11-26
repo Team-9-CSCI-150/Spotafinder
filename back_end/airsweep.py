955023d5c33a0fc58de979255eec36c427972b5f
Trien Ngo <trien.ngo559@gmail.com>
2019-11-26 14:53:38 -0800

    self args

--- back_end/airsweep.py
+++ back_end/airsweep.py
@@ -5,5 +5,18 @@ class airsweep(object):
         self,
         file_prefix='capture'
     ):
+    self.args = {
+        'bssid': self.bssid,
+        'adapter': self.adapter,
+        'channel': self.channel if self.channel else '',
+        'airdump': binaries.airodump_ng,
+        'airmon': binaries.airmon_ng,
+        'aircrack': binaries.aircrack_ng,
+        'wifite': binaries.wifite,
+        'wordlist': self.wordlist,
+    }
+
     def __enter__(self):
         LOG.info("starting monitor mode on {}".format(self.adapter))
+
+    
