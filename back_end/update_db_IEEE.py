a5e957b005fc332306475ce82f014211ab69a3f2
Trien Ngo <trien.ngo559@gmail.com>
2019-12-06 20:12:35 -0800

    csv classes

--- back_end/update_db_IEEE.py
+++ back_end/update_db_IEEE.py
@@ -4,7 +4,19 @@ from random import randint
 import time as ti
 import csv
 from datetime import *
+import panda as pd
 
+import csv
+
+class City:
+        def __init__(self, row, header, the_id):
+                self.__dict__ = dict(zip(header, row)) 
+                self.the_id = the_id
+        def __repr__(self):
+                return self.the_id
+
+data = list(csv.reader(open('file.csv')))
+instances = [City(a, data[0], "city_{}".format(i+1)) for i, a in enumerate(data[1:])]
 
 class updateOccupancy:
         def __init__(self, EEdb, Ldb):
