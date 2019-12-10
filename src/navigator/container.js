7e21725a70780ff36d822184664c63bbfdd99f8f
Khai <zekhai@mail.fresnostate.edu>
2019-12-09 23:34:29 -0800

    initial route

--- src/navigator/container.js
+++ src/navigator/container.js
@@ -20,7 +20,7 @@ export default createAppContainer(
             'Welcome': {screen: Welcome},
         },
         {
-            initialRouteName: 'Setting',
+            initialRouteName: 'Welcome',
         },
     )
 );
