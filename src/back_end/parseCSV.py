99fcbf6cf31711391ab05b7a064bdba29e9c7f80
Trien Ngo <trien.ngo559@gmail.com>
2020-02-11 16:21:45 -0800

    initial commit
    old project getting put in here

--- src/back_end/parseCSV.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import csv
-
-data = pd.read_csv("Users/Vince/Documents/GitHub/Spotafinder/back_end/csvparser/outputSniffTest-01.csv", error_bad_lines=False)
-# data = pd.read_csv("outputSniffTest-01.csv", error_bad_lines=False)
-
-
-print(data.head)
